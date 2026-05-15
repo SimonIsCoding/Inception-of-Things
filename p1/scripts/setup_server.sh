b91450945e93b9007f3984524944ca60c4165090
SimonIsCoding <108285719+SimonIsCoding@users.noreply.github.com>
2026-05-15 11:26:36 +0200

    Update setup_server.sh

--- p1/scripts/setup_server.sh
+++ p1/scripts/setup_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt-get update && apt-get install -y curl && apt-get install -y virtualbox-guest-utils
 curl -sfL https://get.k3s.io | sh -
 sleep 10
 cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
