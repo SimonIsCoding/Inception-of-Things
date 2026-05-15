dcd159b20ae3b8fb330e127f93918feff79f5c74
SimonIsCoding <108285719+SimonIsCoding@users.noreply.github.com>
2026-05-15 11:26:57 +0200

    Update setup_worker.sh

--- p1/scripts/setup_worker.sh
+++ p1/scripts/setup_worker.sh
@@ -6,6 +6,7 @@ done
 NODE_TOKEN=$(cat /vagrant/node-token)
 SERVER_IP="192.168.56.110"
 
+apt-get update && apt-get install -y curl && apt-get install -y virtualbox-guest-utils
 curl -sfL https://get.k3s.io | \
 	K3S_URL="https://${SERVER_IP}:6443" \
 	K3S_TOKEN="${NODE_TOKEN}" \
