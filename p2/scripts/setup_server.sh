4e57d93e07aac2b39aa6926a86fca693a975bca7
SimonIsCoding <108285719+SimonIsCoding@users.noreply.github.com>
2026-05-19 10:24:41 +0200

    updating correct server ip address

--- p2/scripts/setup_server.sh
+++ p2/scripts/setup_server.sh
@@ -5,7 +5,7 @@ apt-get update
 apt-get install -y curl
 
 curl -sfL https://get.k3s.io | \
-  INSTALL_K3S_EXEC="server --node-ip=192.168.52.110 --flannel-iface=eth1" \
+  INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --flannel-iface=eth1" \
   sh -
 
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
